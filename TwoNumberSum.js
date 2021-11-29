c6b0d34d76b7d21609eb29bca6cc3c067d466f04
esrasen9 <esrasen860@gmail.com>
2021-11-29 18:00:15 +0300

    First commit

--- TwoNumberSum.js
+++ TwoNumberSum.js
@@ -1,9 +1,9 @@
 function twoNumberSum(array, targetSum) {
-    for(let i=0; i<array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         const num1 = array[i];
         const num2 = targetSum - num1;
-        if(array.includes(num2) && num1 !== num2){
-            return [num1,num2];
+        if (array.includes(num2) && num1 !== num2) {
+            return [num1, num2];
         }
     }
     return [];
