4ad4778cab4e7e215c0e14cf1034b6b478575509
esrasen9 <esrasen860@gmail.com>
2021-12-09 14:20:49 +0300

    First commit

--- ClassPhotos.js
+++ ClassPhotos.js
@@ -1,19 +1,19 @@
 function classPhotos(redShirtHeights, blueShirtHeights) {
-    redShirtHeights.sort((a,b) => a-b);
-    blueShirtHeights.sort((a,b) => a-b);
-    const firstRowIsBlue = (blueShirtHeights[0] < redShirtHeights[0]) ? true : false;
+   redShirtHeights.sort((a,b) => a-b);
+   blueShirtHeights.sort((a,b) => a-b);
+   const firstRowIsBlue = (blueShirtHeights[0] < redShirtHeights[0]) ? true : false;
 
-	for(let i=0; i < redShirtHeights.length; i++){
-		const red = redShirtHeights[i];
-		const blue = blueShirtHeights[i];
+   for(let i=0; i < redShirtHeights.length; i++){
+ 	  const red = redShirtHeights[i];
+ 	  const blue = blueShirtHeights[i];
 
-		if(firstRowIsBlue){
-			if(blue >= red) return false;
-		}
-		else{
-			if(red >= blue) return false;
-		}
-	}
+ 	  if(firstRowIsBlue){
+ 	     if(blue >= red) return false;
+ 	  }
+ 	  else{
+ 	  	if(red >= blue) return false;
+ 	  }
+   }
 
-	return true;
+   return true;
 }
