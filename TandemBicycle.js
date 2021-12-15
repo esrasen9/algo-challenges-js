eb1ce94cc0828526c576ca3456713461638c6fc4
esrasen9 <esrasen860@gmail.com>
2021-12-15 22:54:53 +0300

    First commit

--- TandemBicycle.js
+++ TandemBicycle.js
@@ -2,7 +2,7 @@ function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
     let result = 0;
     redShirtSpeeds.sort((a, b) => a - b);
     fastest ?
-        blueShirtSpeeds.sort((a, b) => b - a)
+        blueShirtSpeeds.sort((a, b) => b - a);
         : blueShirtSpeeds.sort((a, b) => a - b);
 
     redShirtSpeeds.forEach((red, idx) => {
