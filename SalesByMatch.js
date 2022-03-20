f406b2342191ac7a5280f28205e0e736ea2e76f9
esrasen9 <esrasen860@gmail.com>
2022-03-20 03:31:16 +0300

    Add alternative solution

--- SalesByMatch.js
+++ SalesByMatch.js
@@ -17,18 +17,18 @@ function sockMerchant2(n, ar) {
 
 function sockMerchant(n, ar) {
     let total = 0;
-    const arr2=[];
-    for(let i=0; i<n; i++){
+    const arr2 = [];
+    for (let i = 0; i < n; i++) {
         let temp = 1;
         let sock = ar[i];
-        if(!arr2.includes(sock)){
-            for(let j=0; j<n; j++){
-                if(i !== j && ar[j]===sock){
+        if (!arr2.includes(sock)) {
+            for (let j = 0; j < n; j++) {
+                if (i !== j && ar[j] === sock) {
                     temp++;
                 }
             }
-            if(temp>0){
-                total += parseInt(temp/2);
+            if (temp > 0) {
+                total += parseInt(temp / 2);
             }
             arr2.push(sock);
         }
